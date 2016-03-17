aea2600bc52b77e356a4ff5cc529261d04bcb588
natasavatres <natasavatres@gmail.com>
2016-03-17 14:51:13 +0100

    Dodata logicka kontrola u metodu pronadjiKnjigu klase Biblioteka

--- Biblioteka2016/src/biblioteka/Biblioteka.java
+++ Biblioteka2016/src/biblioteka/Biblioteka.java
@@ -28,6 +28,11 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		
+		if(naslov==null || naslov.isEmpty()){
+			return knjige;
+		}
+		
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
 		
 		for (int i = 0; i < knjige.size(); i++) {
