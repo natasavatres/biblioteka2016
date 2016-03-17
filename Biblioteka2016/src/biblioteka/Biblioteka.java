c4a9246caae1613c0dda34b13bfd97454dd6c037
natasavatres <natasavatres@gmail.com>
2016-03-17 14:47:41 +0100

    Uvedena logicka kontrola u klasu Biblioteka

--- Biblioteka2016/src/biblioteka/Biblioteka.java
+++ Biblioteka2016/src/biblioteka/Biblioteka.java
@@ -10,12 +10,16 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
+		if(knjiga == null || knjige.contains(knjiga))
+			throw new RuntimeException("Greska pri unosu knjige");
 		knjige.add(knjiga);
 
 	}
 
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
+		if(knjiga == null || !knjige.contains(knjiga))
+			throw new RuntimeException("Greska pri brisanju knjige");
 		knjige.remove(knjiga);
 
 	}
@@ -28,6 +32,11 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		
+		if(naslov == null || naslov.isEmpty()){
+			throw new RuntimeException("Naslov je prazan");
+		}
+		
 		LinkedList<Knjiga> rezultat = new LinkedList<Knjiga>();
 		
 		for (int i = 0; i < knjige.size(); i++) {
